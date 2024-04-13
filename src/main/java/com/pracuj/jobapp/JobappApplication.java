eb12b71b419e01edddae1c2ce59a452a9b035451
lifeinline1 <lifeinline1@gmail.com>
2024-04-13 12:46:33 +0200

    Hello git

--- src/main/java/com/pracuj/jobapp/JobappApplication.java
+++ src/main/java/com/pracuj/jobapp/JobappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Git");
 		SpringApplication.run(JobappApplication.class, args);
 	}
 
